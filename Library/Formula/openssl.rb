c02b8b969909b7a15b12712282ec29c584367b05
Michaux Kelley <michauxkelley@gmail.com>
2013-04-10 22:47:56 -0400

    Update URL for openssl.org

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Openssl < Formula
-  homepage 'http://openssl.org'
-  url 'http://openssl.org/source/openssl-1.0.1e.tar.gz'
+  homepage 'http://www.openssl.org'
+  url 'http://www.openssl.org/source/openssl-1.0.1e.tar.gz'
   sha256 'f74f15e8c8ff11aa3d5bb5f276d202ec18d7246e95f961db76054199c69c1ae3'
 
   keg_only :provided_by_osx,
